1b0263018c5957729e9e10b6f6e9f9b8a82f8b05
Billy <billydevbusiness@gmail.com>
2024-12-23 16:20:59 +0530

    feat: String concatenation infix expressions added

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -174,6 +174,8 @@ func evalInfixExpression(
 	switch {
 	case left.Type() == object.INTEGER_OBJ && right.Type() == object.INTEGER_OBJ:
 		return evalIntegerInfixExpression(operator, left, right)
+	case left.Type() == object.STRING_OBJ && right.Type() == object.STRING_OBJ:
+		return evalStringInfixExpression(operator, left, right)
 
 	// other comparison, for now
 	case operator == "==":
@@ -220,6 +222,25 @@ func evalIntegerInfixExpression(
 	default:
 		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
 	}
+}
+
+func evalStringInfixExpression(
+	operator string,
+	left, right object.Object,
+) object.Object {
+	if operator != "+" {
+		return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	}
+
+	leftVal := left.(*object.String).Value
+	rightVal := right.(*object.String).Value
+
+	// switch operator {
+	// case "+":
+	return &object.String{Value: leftVal + rightVal}
+	// default:
+	// 	return newError("unknown operator: %s %s %s", left.Type(), operator, right.Type())
+	// }
 }
 
 func evalIfExpression(ie *ast.IfExpression, env *object.Environment) object.Object {
