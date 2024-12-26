e8d4a0c41276d271846d6d05755b7903e4b003ab
Billy <billydevbusiness@gmail.com>
2024-12-26 18:19:16 +0530

    fix: Fix indexing expression tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -450,7 +450,7 @@ func TestArrayIndexExpressions(t *testing.T) {
 		},
 		{
 			"[1, 2, 3][-1]",
-			nil,
+			3,
 		},
 	}
 
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -15,7 +15,7 @@ let add = fn(x, y) {
 };
 
 let result = add(five, ten);
-!-/*5;
+!-/ *5;
 5 < 10 > 5;
 
 if (5 < 10) {
