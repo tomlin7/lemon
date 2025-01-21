fc69173d67489ee3b255757b2f1fa3e478497a81
Billy <billydevbusiness@gmail.com>
2025-01-22 01:13:57 +0530

    feat: Recording macros in evaluator

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -443,3 +443,21 @@ func evalMapIndexExpression(_map, index object.Object) object.Object {
 
 	return pair.Value
 }
+
+func DefineMacros(program *ast.Program, env *object.Environment) {
+	definitions := []int{}
+
+	for i, statement := range program.Statements {
+		if isMacroDefinition(statement) {
+			addMacro(statement, env)
+			definitions = append(definitions, i)
+		}
+	}
+
+	for i := len(definitions) - 1; i >= 0; i = i - 1 {
+		definitionIndex := definitions[i]
+		program.Statements = append(
+			program.Statements[:definitionIndex],
+			program.Statements[definitionIndex+1:]...)
+	}
+}
---
+++ evaluator/macro_expansion.go
@@ -0,0 +1,33 @@
+package evaluator
+
+import (
+	"lemon/ast"
+	"lemon/object"
+)
+
+func isMacroDefinition(node ast.Statement) bool {
+	letStatement, ok := node.(*ast.LetStatement)
+	if !ok {
+		return false
+	}
+
+	_, ok = letStatement.Value.(*ast.MacroLiteral)
+	if !ok {
+		return false
+	}
+
+	return true
+}
+
+func addMacro(stmt ast.Statement, env *object.Environment) {
+	letStatement, _ := stmt.(*ast.LetStatement)
+	macroLiteral, _ := letStatement.Value.(*ast.MacroLiteral)
+
+	macro := &object.Macro{
+		Parameters: macroLiteral.Parameters,
+		Env:        env,
+		Body:       macroLiteral.Body,
+	}
+
+	env.Set(letStatement.Name.Value, macro)
+}
